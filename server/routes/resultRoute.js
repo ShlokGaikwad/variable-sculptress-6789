d1f6bf593f13ae22818069820880f0b2317d8e81
visorry <vishnuvisa00@gmail.com>
2024-02-20 23:03:04 +0530

    inital model schemas for result and history

--- server/routes/resultRoute.js
+++ server/routes/resultRoute.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const authMiddleware = require('../middleware/auth');
 const Result = require('../models/resultModel');
 
-// Route to get user's quiz results
+// Route to get results
 router.get('/user/:userId', authMiddleware, async (req, res) => {
   try {
     const userId = req.params.userId;
-    const results = await Result.find({ userId }).populate('quizId');
+    const results = await Result.find({ userId });
     res.json(results);
   } catch (error) {
     console.error(error);
@@ -15,37 +15,35 @@ router.get('/user/:userId', authMiddleware, async (req, res) => {
   }
 });
 
+// Route to add result
 router.post('/add', authMiddleware, async (req, res) => {
   try {
     const { quizId, questions, totalScore, correctCount, incorrectCount } = req.body;
-    const userId = req.user.id; // from auth jwt middleware
+    const userId = req.user.id; // From jwt auth middelware
 
     const newResult = new Result({
-      resultId: generateUniqueResultId(), // You need to implement a function to generate unique result IDs
       userId,
       quizId,
       questions,
       totalScore,
       correctCount,
       incorrectCount,
-      // Add other fields as needed
     });
 
-    await newResult.save();
-    res.json({ msg: 'Quiz result added successfully' });
+    const savedResult = await newResult.save();
+    res.json({ msg: 'Quiz result added successfully', result: savedResult });
   } catch (error) {
     console.error(error);
     res.status(500).json({ msg: 'Server Error' });
   }
 });
 
-// Route to delete a quiz result by resultId
+// Route to delete result
 router.delete('/:resultId', authMiddleware, async (req, res) => {
   try {
     const resultId = req.params.resultId;
     const userId = req.user.id;
 
-    // Check if the result belongs to the authenticated user
     const result = await Result.findOne({ _id: resultId, userId });
 
     if (!result) {
@@ -59,10 +57,5 @@ router.delete('/:resultId', authMiddleware, async (req, res) => {
     res.status(500).json({ msg: 'Server Error' });
   }
 });
-
-// Function to generate a unique result ID (you may use a more robust method)
-function generateUniqueResultId() {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
-}
 
 module.exports = router;
