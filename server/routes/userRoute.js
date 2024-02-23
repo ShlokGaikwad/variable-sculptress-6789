f2c7783e1098ded418a5018797b66404e5020d9a
visorry <vishnuvisa00@gmail.com>
2024-02-24 04:29:58 +0530

    lttle tweaks on admin panel

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -25,6 +25,21 @@ userRouter.get("/user/:userId", async (req, res) => {
   }
 });
 
+userRouter.get("/user", async (req, res) => {
+
+  try {
+    const users = await UserModel.find();
+
+    if (!users) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json( users );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 userRouter.post("/signup", async (req, res, next) => {
   const { username, email, password } = req.body;
 
