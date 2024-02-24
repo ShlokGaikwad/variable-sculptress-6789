8e81186996e0888eebc0f6e1ff4043091eee94e0
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2024-02-24 16:43:38 +0530

    fetch req for get and post

--- client/Admin Dashboard Panel/adminPages/analytics.js
+++ client/Admin Dashboard Panel/adminPages/analytics.js
@@ -2,11 +2,36 @@ const totalLanguage = document.getElementById("totalLanguage");
 const totalQuestion = document.getElementById("totalQuestion");
 
 const url = "https://variable-sculptress-6789-e41a.onrender.com";
-
-const fetch = async () => {
+let token = localStorage.getItem("token");
+console.log(token);
+const fetchData = async (endpoint) => {
   try {
-    const res = await fetch(`${url}`);
+    const res = await fetch(`${url}/${endpoint}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    });
+    const data = await res.json();
+    console.log(data);
+    return data;
   } catch (error) {
     console.log(error);
   }
 };
+
+fetchData("languages")
+  .then((data) => {
+    totalLanguage.innerHTML = data.length;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+fetchData("questions")
+  .then((data) => {
+    totalQuestion.innerHTML = data.length;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
