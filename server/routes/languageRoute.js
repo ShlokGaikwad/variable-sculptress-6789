522f54688de7d4da90e70d9322d53bce7309e6dd
Shlok gaikwad <shlokgaikwad8888@gmail.com>
2024-02-25 16:52:57 +0530

    dashboard issue solved

--- server/routes/languageRoute.js
+++ server/routes/languageRoute.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Language = require('../models/languageModel');
-const uploadMiddleware = require('../middleware/uploadImage');
+const Language = require("../models/languageModel");
+const uploadMiddleware = require("../middleware/uploadImage");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const languages = await Language.find();
-    res.status(200).json(languages); 
+    res.status(200).json(languages);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' }); 
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.get('/:languageId', async (req, res) => {
+router.get("/:languageId", async (req, res) => {
   const { languageId } = req.params;
 
   try {
-    const language = await Language.find({_id : languageId });
+    const language = await Language.find({ _id: languageId });
 
     if (!language) {
-      return res.status(404).json({ error: 'Language not found' });
+      return res.status(404).json({ error: "Language not found" });
     }
 
     res.status(200).json(language);
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' }); 
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-router.post('/add', uploadMiddleware('languageImage'), async (req, res) => {
+router.post("/add", uploadMiddleware("languageImage"), async (req, res) => {
   const { languageTitle } = req.body;
-  const languageImage = req.imagePath; 
+  const languageImage = req.imagePath;
 
   try {
     const newLanguage = new Language({
@@ -42,10 +42,9 @@ router.post('/add', uploadMiddleware('languageImage'), async (req, res) => {
 
     res.status(201).json(newLanguage);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(400).json({ error: 'Bad Request' });
+    console.error("Error:", error);
+    res.status(400).json({ error: "Bad Request" });
   }
 });
-
 
 module.exports = router;
